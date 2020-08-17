5e241614f3c4e641118545467c2c68273531ea4e
Andreas Jaggi <andreas.jaggi@waterwave.ch>
2020-08-17 22:18:45 +0200

    Add support for custom patterns

--- crawlerdetect.go
+++ crawlerdetect.go
@@ -8,15 +8,74 @@
 //             fmt.Println("Found a crawler")
 //     }
 //
+// To use custom patterns a dedicated instance can be created:
+//
+//     uastring := "curl/7.54.0"
+//     crawlerDetect := crawlerDetect.New()
+//     crawlerDetect.SetCrawlers([]string{`curl`})
+//     if crawlerDetect.IsCrawler(uastring) {
+//             fmt.Println("Found a crawler")
+//     }
+//
 package crawlerdetect
 
-import "strings"
+import (
+	"regexp"
+	"strings"
+)
 
-func excludeString(input string) string {
-	return exclusionsRegex.ReplaceAllString(strings.TrimSpace(input), "")
+var defaultCrawlerDetect *CrawlerDetect
+
+func init() {
+	defaultCrawlerDetect = New()
 }
 
 // IsCrawler checks for a user agent string if it is a crawler
 func IsCrawler(input string) bool {
-	return crawlersRegex.MatchString(excludeString(input))
+	return defaultCrawlerDetect.IsCrawler(input)
+}
+
+// CrawlerDetect contains the patterns and exclusions for detecting crawlers
+type CrawlerDetect struct {
+	crawlers        []string
+	crawlersRegex   *regexp.Regexp
+	exclusions      []string
+	exclusionsRegex *regexp.Regexp
+}
+
+// New creates a new CrawlerDetect instance with the default patterns
+func New() *CrawlerDetect {
+	c := new(CrawlerDetect)
+	c.SetCrawlers(crawlers)
+	c.SetExclusions(exclusions)
+	return c
+}
+
+// SetCrawlers sets a custom list of crawler patterns
+func (c *CrawlerDetect) SetCrawlers(crawlers []string) {
+	c.crawlers = crawlers
+	c.compileCrawlers()
+}
+
+// SetExclusions sets a custom list of exclusion patterns
+func (c *CrawlerDetect) SetExclusions(exclusions []string) {
+	c.exclusions = exclusions
+	c.compileExclusions()
+}
+
+func (c *CrawlerDetect) compileCrawlers() {
+	c.crawlersRegex = regexp.MustCompile("(?i)(" + strings.Join(c.crawlers, "|") + ")")
+}
+
+func (c *CrawlerDetect) compileExclusions() {
+	c.exclusionsRegex = regexp.MustCompile("(?i)(" + strings.Join(c.exclusions, "|") + ")")
+}
+
+func (c *CrawlerDetect) excludeString(input string) string {
+	return c.exclusionsRegex.ReplaceAllString(strings.TrimSpace(input), "")
+}
+
+// IsCrawler checks for a user agent string if it is a crawler
+func (c *CrawlerDetect) IsCrawler(input string) bool {
+	return c.crawlersRegex.MatchString(c.excludeString(input))
 }
--- update_data.sh
+++ update_data.sh
@@ -16,20 +16,20 @@ BEGIN{
 END{
 	printf("}\n")
 }
-' > "${name}_data.go"
+' > "${name}.go"
 }
-echo "Updating crawlers_data.go"
+echo "Updating crawlers.go"
 datafile "https://raw.githubusercontent.com/JayBizzle/Crawler-Detect/master/raw/Crawlers.txt" "crawlers"
-echo "Updating exclusions_data.go"
+echo "Updating exclusions.go"
 datafile "https://raw.githubusercontent.com/JayBizzle/Crawler-Detect/master/raw/Exclusions.txt" "exclusions"
 
 echo "Patching files for re2 engine"
 # XXX: golang re2 based regex engine does not support negative lookahead Yandex(?!Search)
 # as workaround add YandexSearch to the exclusions list and add Yandex to the crawlers list
-sed -i "" -e 's/`Yandex(?!Search)`/`Yandex`/' crawlers_data.go
+sed -i "" -e 's/`Yandex(?!Search)`/`Yandex`/' crawlers.go
 sed -i "" -e '/`;`/i\
-	`YandexSearch`,' exclusions_data.go
-sed -i "" -e 's/`YandexSearch`,/	&/' exclusions_data.go
+	`YandexSearch`,' exclusions.go
+sed -i "" -e 's/`YandexSearch`,/	&/' exclusions.go
 
 echo "Updating testdata/crawlers.txt"
 curl --progress-bar -o testdata/crawlers.txt https://raw.githubusercontent.com/JayBizzle/Crawler-Detect/master/tests/crawlers.txt
